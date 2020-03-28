b6b9c5f3e1afb0b8feadabd6b33398cd90f663d7
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2020-03-28 16:41:35 +0100

    initial

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,17 @@
-const router = require('express').Router();
+const bc = require("bcryptjs");
+const router = require("express").Router();
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
+
+  const { username, password } = req.body;
+  const hashPassword = bc.hashSync(password, 10);
+  console.log("register endpoint");
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
+  console.log(" login endpoint");
 });
 
 module.exports = router;
