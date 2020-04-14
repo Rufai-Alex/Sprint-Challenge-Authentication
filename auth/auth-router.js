d26262248942ba0368ccf19d576de4b69cd8a2b3
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2020-04-14 04:15:37 +0100

    register-login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,17 +1,59 @@
-const bc = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const db = require("../database/dbConfig.js");
+
+const { jwtSecret } = require("./secret.js");
+const Users = require("./auth-model.js");
 
 router.post("/register", (req, res) => {
   // implement registration
 
-  const { username, password } = req.body;
-  const hashPassword = bc.hashSync(password, 10);
-  console.log("register endpoint");
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 3);
+  user.password = hash;
+
+  Users.add(user)
+    .then((newUser) => {
+      res.status(201).json(newUser);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "There was an error" });
+    });
 });
 
 router.post("/login", (req, res) => {
   // implement login
-  console.log(" login endpoint");
+  let { username, password } = req.body;
+
+  Users.findByUser(username)
+    .first()
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = signToken(user);
+
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "There was an error" });
+    });
 });
 
+// TOKEN
+function signToken(user) {
+  const payload = {
+    id: user.id,
+    name: user.username,
+  };
+
+  const options = {
+    expiresIn: "8h",
+  };
+  return jwt.sign(payload, jwtSecret, options);
+}
 module.exports = router;
