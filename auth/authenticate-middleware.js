5bc8377f08c0045ca6689e46b19bc7c9e8fa23b5
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2020-04-17 11:31:11 +0100

    API

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,25 @@
-/* 
+const jwt = require("jsonwebtoken");
+
+const { jwtSecret } = require("./secret.js");
+
+/*
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Must be an authorized user" });
+      } else {
+        req.user = decodedToken;
+      }
+      next();
+    });
+  } else {
+    res.status(401).json({ message: "Must be an authorized user" });
+  }
 };
